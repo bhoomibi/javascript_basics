c8ea050e8e03908eb45a75c5fdd8a6b796763a09
bhoomibi <115976954+bhoomibi@users.noreply.github.com>
2024-12-12 10:50:47 +0000

    functions done

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,10 @@
-console.log("hello")
+let aboin=8930
+const abel="Hemani"
+var hro="trueil"
+kalds=09823
+
+aboin=0988
+// abel="truu"
+hro="kkf"
+kalds="trueeed"
+console.table([aboin,abel,hro,kalds])
